68064f3ae37c00d90ad50cad3bb2a96dc655d799
Epaphras168 <ishimweepa18@gmail.com>
2025-11-25 00:59:18 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // replace with your actual ngrok host (it changes every time unless you reserve one)
 export default defineConfig({
   plugins: [react()],
-  base: '/Attendance',
+  base: '/Attendance/',
   server: {
     allowedHosts: [
       '87e4a702d500.ngrok-free.app', // ngrok domain
